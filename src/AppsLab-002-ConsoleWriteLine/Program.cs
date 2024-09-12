1dc3d1d0df938399553d585b181b0ae15c6660d8
matusbeke <matus.beke7@gmail.com>
2024-09-12 09:16:43 +0200

    Konzolu som napisal aby vypisala moje meno :D

--- src/AppsLab-002-ConsoleWriteLine/Program.cs
+++ src/AppsLab-002-ConsoleWriteLine/Program.cs
@@ -1,1 +1,10 @@
 ﻿Console.WriteLine("Hello, World!");
+
+string greetings = "Ahoj svet!";
+Console.WriteLine(greetings);
+
+greetings = "Dovidenia svet!";
+Console.WriteLine(greetings);
+
+string name = "Matus!";
+Console.WriteLine("Hello " + name);
