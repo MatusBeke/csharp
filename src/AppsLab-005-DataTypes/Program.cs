c513975f1f83ea20ee20627a178ed33e78561c88
matusbeke <matus.beke7@gmail.com>
2024-09-17 08:13:13 +0200

    something about me :D

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int age = 15;
+string name = "Matus";
+char firstLetter = 'M';
+bool isAdult = false;
+float accountBallance = 0;
+
+Console.WriteLine(name);
+Console.WriteLine("First letter of name is: " + firstLetter);
+Console.WriteLine("Ia adult: " + isAdult);
+Console.WriteLine("Account ballance: " + accountBallance);
+Console.WriteLine(age);
