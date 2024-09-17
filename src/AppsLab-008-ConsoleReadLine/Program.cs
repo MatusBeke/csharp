179125331754dc6cb21024b5fe16a780b1d88cfc
matusbeke <matus.beke7@gmail.com>
2024-09-17 10:08:20 +0200

    kalkulacia

--- src/AppsLab-008-ConsoleReadLine/Program.cs
+++ src/AppsLab-008-ConsoleReadLine/Program.cs
@@ -1,4 +1,4 @@
-﻿//tvoje meno
+﻿/*//tvoje meno
 Console.WriteLine("Ako sa voláš?");
 string? meno = Console.ReadLine();
 
@@ -12,4 +12,35 @@ Console.WriteLine("\nAké je tvoje obľúbené jedlo?");
 string? jedlo = Console.ReadLine();
 
 //napise vetu o tebe
-Console.WriteLine("Ahoj,tvoje meno je " + meno + " máš " + vek + " rokov a tvoje oblúbené jedlo je " + jedlo + ".");
+Console.WriteLine("Ahoj,tvoje meno je " + meno + " máš " + vek + " rokov a tvoje oblúbené jedlo je " + jedlo + ".");*/
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//Operacie 
+int prveCislo  = 41;
+int druheCislo  = 80;
+
+Console.WriteLine(prveCislo + druheCislo);
+Console.WriteLine(prveCislo - druheCislo);
+Console.WriteLine(prveCislo * druheCislo);
+Console.WriteLine(prveCislo / druheCislo);
+
+Console.WriteLine();
+
+Console.WriteLine(10 == 50);
+Console.WriteLine(50 == 55);
+
+Console.WriteLine();
+
+Console.WriteLine(10 != 50);
+Console.WriteLine(50 != 55);
+
+Console.WriteLine();
+
+Console.WriteLine(10 < 50);
+Console.WriteLine(50 > 55);
+
+Console.WriteLine();
+
+Console.WriteLine(10 <= 50);
+Console.WriteLine(50 >= 55);
