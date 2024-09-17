11d6b35098a32929666b9f193281320281e6997b
matusbeke <matus.beke7@gmail.com>
2024-09-17 09:10:06 +0200

    otazky na teba :D

--- src/AppsLab-008-ConsoleReadLine/Program.cs
+++ src/AppsLab-008-ConsoleReadLine/Program.cs
@@ -1,8 +1,15 @@
-﻿Console.WriteLine("Ako sa voláš?");
+﻿//tvoje meno
+Console.WriteLine("Ako sa voláš?");
 string? meno = Console.ReadLine();
 Console.WriteLine("Ahoj, " + meno);
 
-Console.WriteLine("Koľko máš rokov?");
+//kolko mas rokov
+Console.WriteLine("\nKoľko máš rokov?");
 string vstup = Console.ReadLine() ?? "0";
 int vek = int.Parse(vstup);
 Console.WriteLine("Máš " + vek + " rokov.");
+
+//oblubene jedlo
+Console.WriteLine("\nAké je tvoje obľúbené jedlo?");
+string? jedlo = Console.ReadLine();
+Console.WriteLine("Mmm " + jedlo + " to znie ako Fifik na hajzli.");
