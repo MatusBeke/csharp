c2d3363143dbab9e08122e163db92f529a2ca801
matusbeke <matus.beke7@gmail.com>
2024-09-17 10:46:46 +0200

    hesla

--- src/AppsLab-008-ConsoleReadLine/Program.cs
+++ src/AppsLab-008-ConsoleReadLine/Program.cs
@@ -17,7 +17,7 @@ Console.WriteLine("Ahoj,tvoje meno je " + meno + " máš " + vek + " rokov a tvo
 //--------------------------------------------------------------------------------------------------------------------------------------------------------
 
 //Operacie 
-int prveCislo  = 41;
+/*int prveCislo  = 41;
 int druheCislo  = 80;
 
 Console.WriteLine(prveCislo + druheCislo);
@@ -44,3 +44,41 @@ Console.WriteLine();
 
 Console.WriteLine(10 <= 50);
 Console.WriteLine(50 >= 55);
+
+Console.WriteLine();*/
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+var vysledok = "55" + "10";
+Console.WriteLine(vysledok);
+
+//databazy
+var menoPouzivatela = "MatusBeke";
+var hesloPouzivatela = "123456789";
+
+//Zadanie uzivatelom
+var zadanieMeno = "MatusBeke";
+var zadanieHeslo = "123456789";
+
+//porovnanie
+var porovnanieMeno = menoPouzivatela == zadanieMeno;
+var porovnanieHeslo = hesloPouzivatela == zadanieHeslo;
+
+var prijatyPristup = porovnanieHeslo && porovnanieMeno;
+
+//vystup
+Console.WriteLine("Meno zhoda: " + porovnanieMeno);
+Console.WriteLine("Heslo zhoda: " + porovnanieHeslo);
+Console.WriteLine("Povoleny pristup: " + prijatyPristup);
+
+if (prijatyPristup)
+{
+    Console.WriteLine("Heslo sa zhoduje, vitajte.:D " + menoPouzivatela);
+}
+else
+{
+    Console.WriteLine("Vase heslo nie je spravne. :C");
+}
