9a1d078668f6b730b507a7b1521e85e22dfb0531
matusbeke <matus.beke7@gmail.com>
2024-09-17 09:17:19 +0200

    + cela vete o tebe :D

--- src/AppsLab-008-ConsoleReadLine/Program.cs
+++ src/AppsLab-008-ConsoleReadLine/Program.cs
@@ -1,15 +1,15 @@
 ﻿//tvoje meno
 Console.WriteLine("Ako sa voláš?");
 string? meno = Console.ReadLine();
-Console.WriteLine("Ahoj, " + meno);
 
 //kolko mas rokov
 Console.WriteLine("\nKoľko máš rokov?");
 string vstup = Console.ReadLine() ?? "0";
 int vek = int.Parse(vstup);
-Console.WriteLine("Máš " + vek + " rokov.");
 
 //oblubene jedlo
 Console.WriteLine("\nAké je tvoje obľúbené jedlo?");
 string? jedlo = Console.ReadLine();
-Console.WriteLine("Mmm " + jedlo + " to znie ako Fifik na hajzli.");
+
+//napise vetu o tebe
+Console.WriteLine("Ahoj,tvoje meno je " + meno + " máš " + vek + " rokov a tvoje oblúbené jedlo je " + jedlo + ".");
