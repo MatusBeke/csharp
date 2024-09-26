962e385f740c7dbcec5e8a067e7a9c465ead35ca
matusbeke <matus.beke7@gmail.com>
2024-09-26 08:52:34 +0200

    Hotovo :D

--- Jednoduchá-Kalkulačka-035/Program.cs
+++ Jednoduchá-Kalkulačka-035/Program.cs
@@ -8,32 +8,56 @@ namespace MyApp
         {
             Console.WriteLine("Zadajte znak pre matematicku operaciu:");
             string operation = Console.ReadLine();
+            Console.WriteLine("Zadaj prve cislo:");
             string number1 = Console.ReadLine();
+            Console.WriteLine("Zadaj druhe cislo:");
             string number2 = Console.ReadLine();
 
             double number1final = Double.Parse(number1);
             double number2final = Double.Parse(number2);
 
-            if (operation == "+")
+            /* if (operation == "+")
+             {
+                 Console.WriteLine(number1final + number2final);
+             }
+             else if (operation == "-")
+             {
+                 Console.WriteLine(number1final - number2final);
+             }
+             else if (operation == "*")
+             {
+                 Console.WriteLine(number1final * number2final);
+             }
+             else if (operation == "/")
+             {
+                 Console.WriteLine(number1final / number2final);
+             }
+             else
+             {
+                 Console.WriteLine("Nespravne zadany operator!!!");
+             }*/
+
+            Console.WriteLine("Chybne zadane udaje!");
+
+            switch (operation)
             {
-                Console.WriteLine(number1final + number2final);
-            }
-            else if (operation == "-")
-            {
-                Console.WriteLine(number1final - number2final);
-            }
-            else if (operation == "*")
-            {
-                Console.WriteLine(number1final * number2final);
-            }
-            else if (operation == "/")
-            {
-                Console.WriteLine(number1final / number2final);
-            }
-            else
-            {
-                Console.WriteLine("Nespravne zadany operator!!!");
+                case "+":
+                    Console.WriteLine(number1final + number2final);
+                    break;
+                case "-":
+                    Console.WriteLine(number1final + number2final);
+                    break;
+                case "*":
+                    Console.WriteLine(number1final + number2final);
+                    break;
+                case "/":
+                    if (number2final == 0) {
+                        Console.WriteLine("Neda sa delit nulou!");
+                    }
+                    Console.WriteLine(number1final + number2final);
+                    break;
             }
         }
     }
 }
+
