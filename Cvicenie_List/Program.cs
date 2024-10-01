628a759c73ac063c7bb5e1f694c7561f04e56be4
matusbeke <matus.beke7@gmail.com>
2024-10-01 08:47:41 +0200

    :D

---
+++ Cvicenie_List/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+            numbers[0] = 10;
+            numbers[1] = 15;
+            numbers[2] = 20;
+            numbers[3] = 25;
+            numbers[4] = 45;
+
+            Console.WriteLine(numbers[2] + numbers[4]);
+
+            numbers[4] = 10;
+
+            Console.WriteLine(numbers[2] + numbers[4]);
+        }
+    }
+}
