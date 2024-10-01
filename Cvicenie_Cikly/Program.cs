0288908ac855bf733a30cfa413bd54bca91cde93
matusbeke <matus.beke7@gmail.com>
2024-10-01 10:03:09 +0200

    Hotovo velo sa zmenilo :D

---
+++ Cvicenie_Cikly/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string zadajcislo = Console.ReadLine();
+            int konecnecislo = int.Parse(zadajcislo);
+
+            int i = 0;
+            while (i < konecnecislo)
+            {
+                Console.WriteLine(i);
+                i++;  
+            }
+        }
+    }
+}
--- Cvicenie_List/Program.cs
+++ Cvicenie_List/Program.cs
@@ -5,8 +5,7 @@ namespace MyApp
     internal class Program
     {
         static void Main(string[] args)
-        {
-            int[] numbers = new int[5];
+        {           /* int[] numbers = new int[5];
             numbers[0] = 10;
             numbers[1] = 15;
             numbers[2] = 20;
@@ -18,6 +17,33 @@ namespace MyApp
             numbers[4] = 10;
 
             Console.WriteLine(numbers[2] + numbers[4]);
+
+            List<int> listNumbers = new List<int>();
+
+            listNumbers.Add(10);
+            listNumbers.Add(15);
+            listNumbers.Add(20);
+            listNumbers.Add(25);
+            listNumbers.Add(45);
+
+            listNumbers[1] = 1000000; */
+
+            List<string> listMena = new List<string>();
+
+            listMena.Add("Matus");
+            listMena.Add("Jakub");
+            listMena.Add("Marek");
+            listMena.Add("Juraj");
+            listMena.Add("Mato");
+            listMena.Add("Jano");
+
+            Console.WriteLine(listMena[0]);
+            Console.WriteLine(listMena[1]);
+            Console.WriteLine(listMena[2]);
+            Console.WriteLine(listMena[3]);
+            Console.WriteLine(listMena[4]);
+            Console.WriteLine(listMena[5]);
         }
     }
 }
+
