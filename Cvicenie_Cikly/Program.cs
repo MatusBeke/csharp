16eb941929a0f5131c79b67b1d3020ad88acb024
matusbeke <matus.beke7@gmail.com>
2024-10-01 11:06:59 +0200

    du

--- Cvicenie_Cikly/Program.cs
+++ Cvicenie_Cikly/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32.SafeHandles;
+using System;
 using System.Drawing;
+using System.Runtime.InteropServices;
 
 namespace MyApp
 {
@@ -7,15 +9,42 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            string zadajcislo = Console.ReadLine();
-            int konecnecislo = int.Parse(zadajcislo);
+            /* string zadajcislo = Console.ReadLine();
+             int konecnecislo = int.Parse(zadajcislo);
 
-            int i = 0;
-            while (i < konecnecislo)
+             int i = 0;
+             while (i < konecnecislo)
+             {
+                 Console.WriteLine(i);
+                 i++;  
+
+             }*/
+
+            List<string> listMena = new List<string>();
+
+            listMena.Add("Matus");
+            listMena.Add("Jakub");
+            listMena.Add("Marek");
+            listMena.Add("Juraj");
+            listMena.Add("Mato");
+            listMena.Add("Jano");
+
+
+            int counter = 1;
+            foreach (string mena in listMena)
             {
-                Console.WriteLine(i);
-                i++;  
+                Console.WriteLine(counter.ToString().PadLeft(3, '0') + ". "+ mena);
+                counter++;
             }
+             /*for (int i = 0; i < 10; i++)
+            {
+                string riadok = "";
+                for (int x = 0; x < i; x++) 
+                {
+                    riadok += "*";
+                }
+                Console.WriteLine(riadok);
+            }*/
         }
     }
 }
---
+++ uloha zoznam/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> menaList = new List<string>();
+            Console.WriteLine("Vsetky mena v zozname:");
+
+            Console.WriteLine("Zadaj meno:");
+
+            while (true)
+            {
+                string Meno = Console.ReadLine();
+                if (Meno == "Quit")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                    menaList.Add(Meno);
+                    Console.WriteLine("Mena v zozname:");
+                    foreach
+                    menaList.Add(Meno);
+                    
+                }
+            }
+        }
+    }
+}
