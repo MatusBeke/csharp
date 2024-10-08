19424d91116f5f653b2e97806b36ebac5fae81c9
matusbeke <matus.beke7@gmail.com>
2024-10-08 08:31:33 +0200

    Hotovo:D

--- Cvicenie-Zaklady/Program.cs
+++ Cvicenie-Zaklady/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Data;
 
-namespace MyApp
+/*namespace MyApp
 {
     internal class Program
     {
@@ -19,6 +19,42 @@ namespace MyApp
             {
                return text.Length;
             }
-        //DU: spocita samohlasky aj rozlisovat mal=e a velke pismena
+        //DU: spocita samohlasky aj rozlisovat male a velke pismena
+        }
+    }*/
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pocetSamohlasok = 0;
+
+            Console.WriteLine("Napis text:");
+            string text = Console.ReadLine() ?? "0";
+
+            int length = SpocitajPismena(text);
+
+            Char[] samohlasky = text.ToCharArray();
+
+            foreach (char c in samohlasky)
+            {
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+                {
+                   pocetSamohlasok++;
+                }
+            }
+
+
+
+            Console.WriteLine("Text je dlhy: " + text.Length + " pismen");
+            Console.WriteLine("Text obsahuje " + pocetSamohlasok + " samohlasok");
+        }
+
+
+        public static int SpocitajPismena(string text)
+        {
+            return text.Length;
         }
     }
+}
