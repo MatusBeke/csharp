aac4121a4c37114c52cadd92a757934f9f7e0e45
matusbeke <matus.beke7@gmail.com>
2024-10-10 08:24:03 +0200

    Hotovo :D

---
+++ hra/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+       public static void Main(string[] args)
+        {
+            int pokusy = 1; 
+            Random rnd = new Random();
+            int vygenerovaneCislo = rnd.Next(maxValue: 100);
+
+            bool Game = true;
+
+            while (Game == true) 
+            {
+                Console.WriteLine("Pokusy:" + pokusy);
+                Console.WriteLine("Uhadni cislo:");
+                string hadaneCislo = Console.ReadLine() ?? "0";
+                int hadaneCisloint = int.Parse(hadaneCislo);
+
+                if (hadaneCisloint == vygenerovaneCislo)
+                {
+                    Game = false;
+                    Console.WriteLine("Vyhrali ste!");
+                    pokusy++;
+                }
+                else if (hadaneCisloint > vygenerovaneCislo)
+                {
+                    Console.WriteLine("Vygenerovane cislo je mensie!");
+                    pokusy++;
+                }
+                else if (hadaneCisloint < vygenerovaneCislo)
+                {
+                    Console.WriteLine("Vygenerovane cislo je vacsie!");
+                    pokusy++;
+                }
+            }
+        }
+    }
+}
---
+++ nowa aplikacia obchod/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> store = new List<string>();
+
+            bool isEnd = false; 
+            while (!isEnd)
+            {
+            PrintMenu();
+            var answer = Console.ReadLine();
+
+                switch (answer)
+                {
+                    case "1":
+                        foreach (var item in store)
+                        {
+                            Console.WriteLine(item);    
+                        }
+                        break;
+
+
+                    case "2":
+                        Console.WriteLine("Zadajte meno noveho predmetu: ");
+                        var newItemName = Console.ReadLine() ?? "0";
+                        Console.WriteLine("Zadajte kolko kusov noveho predmetu");
+                        var newItemCount = Console.ReadLine() ?? "0";
+                        AddItem(store, newItemName,Int32.Parse(newItemCount));
+                        break;
+
+                    case "3":
+                        isEnd = true;
+                        break;
+        
+                    default:
+                        Console.WriteLine("Nespravna akcia!");
+                        break;
+                }
+            }
+        }
+
+        public static void AddItem(List<string> itemlist, string itemName, int itemCount)
+        {
+            itemlist.Add(itemName);
+        }
+
+        public static void PrintMenu()
+        {
+            Thread.Sleep(3000);
+            Console.Clear();
+            Console.WriteLine("1.Vypis poloziek");
+            Console.WriteLine("2.Pridane polozky");
+            Console.WriteLine("4.Exit");
+            Console.Write("Vyberte akciu:");
+        }
+    }
+}
