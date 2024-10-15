2662356d227bdc9b835ec8814f298bd99718f4e7
matusbeke <matus.beke7@gmail.com>
2024-10-15 10:51:32 +0200

    Hotovo :D

--- Cvicenie_OOP_Hra/Player.cs
+++ Cvicenie_OOP_Hra/Player.cs
@@ -9,15 +9,29 @@ namespace Cvicenie_OOP_Hra
     public class Player
     {
         public string Name { get; set; }
-        public int HP { get; set; }
         public int AttackPower { get; set; }
+        public int HP { get; set; }
         public int Mana { get; set; }
+
+        public Player()
+        {
+
+        }
+
+        public Player(string name, int attackPower, int hP, int mana)
+        {
+            Name = name;
+            AttackPower = attackPower;
+            HP = hP;
+            Mana = mana;
+        }
 
         public void DamagePlayer(Player player)
         {
             var HPofEnemy = player.HP;
             int AttackOfCurrentPlayer = this.AttackPower;
             int HPofEnemyAfterFight = HPofEnemy - AttackOfCurrentPlayer;
+            player.HP = HPofEnemyAfterFight;
         }
     }
 }
--- Cvicenie_OOP_Hra/Program.cs
+++ Cvicenie_OOP_Hra/Program.cs
@@ -4,8 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Player player1 = new Player("Bača Jano", 20000, 150, 30000);
-            Player player2 = new Player("Zemepán Nikolaj Palfi", 10, 1950, 60);
+            Player player1 = new Player("Bača Jano", 150, 1500, 40);
+            Player player2 = new Player("Zemepán Nikolaj Palfi", 100, 1950, 60);
+
+            while (player2.HP >= 0 && player1.HP >= 0) 
+            {
+                Console.WriteLine("Player2 HP: " + player2.HP);
+                player1.DamagePlayer(player2);
+                Console.WriteLine("Player2 HP afret Fight: " + player2.HP);
+
+                Console.WriteLine("Player1 HP: " + player1.HP);
+                player2.DamagePlayer(player1);
+                Console.WriteLine("Player1 HP afret Fight: " + player1.HP);
+            }
+            if (player1.HP >= player2.HP)
+            {
+                Console.WriteLine("Player1 is winner!");
+            }
+            else
+            {
+                Console.WriteLine("Player2 is winner");
+            }
         }
     }
 }
