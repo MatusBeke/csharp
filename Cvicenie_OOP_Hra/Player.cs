bbf677072871487d1265cc3d5bc4e8a564b8d396
matusbeke <matus.beke7@gmail.com>
2024-10-17 09:18:44 +0200

    Hotovo :D

--- Cvicenie_OOP_Hra/Player.cs
+++ Cvicenie_OOP_Hra/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,33 +13,72 @@ namespace Cvicenie_OOP_Hra
         public int AttackPower { get; set; }
         public int HP { get; set; }
         public int Mana { get; set; }
+        public int CritChange { get; set; }
+        public int RefilManaChance { get; set; }
 
         public Player()
         {
 
         }
 
-        public Player(string name, int attackPower, int hP, int mana)
+        public Player(string name, int attackPower, int hP, int mana, int CriticalChange, int refilManaChance)
         {
             Name = name;
             AttackPower = attackPower;
             HP = hP;
             Mana = mana;
+            CritChange = CriticalChange;
+            RefilManaChance = refilManaChance;
         }
+
 
         public void DamagePlayer(Player player)
         {
+            int damageMultipayer = 1;
+            Random random = new Random();
+            int randomNumber = random.Next(0, 100);
+            if (randomNumber <= this.CritChange)
+            {
+                damageMultipayer = 2;
+                Console.WriteLine(this.Name + " dal kriticky zasah!");
+            }
+
             var HPofEnemy = player.HP;
             int AttackOfCurrentPlayer = this.AttackPower;
             int HPofEnemyAfterFight = HPofEnemy - AttackOfCurrentPlayer;
             player.HP = HPofEnemyAfterFight;
 
         }
-        public void HealPlayer(Player player)
+        public void RefilMana(int newMana)
         {
-            int ManaOfPlayer = this.Mana;
-            int HPofPlayerPlusMana = this.HP + player.Mana;
-            this.HP = HPofPlayerPlusMana;
+            this.Mana += newMana;
+        }
+        public bool Heal()
+        {
+           
+           Random random = new Random();
+           int randomNumber = random.Next(0, 100);
+            if (randomNumber <= this.RefilManaChance)
+            {
+                this.Mana += 50;
+                Console.WriteLine(this.Name + " Pridal si Manu!");
+            }
+
+
+            if (this.Mana > 0)
+            {
+                int HpOfPlayer = this.HP;
+                int ManaOfPlayer = this.Mana;
+                int HPofPlayerPlusMana = HpOfPlayer + ManaOfPlayer;
+                this.HP = HPofPlayerPlusMana;
+                this.Mana = 0;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+   
         }
     }
 }
--- Cvicenie_OOP_Hra/Program.cs
+++ Cvicenie_OOP_Hra/Program.cs
@@ -4,27 +4,59 @@
     {
         static void Main(string[] args)
         {
-            Player player1 = new Player("Bača Jano", 150, 1500, 60);
-            Player player2 = new Player("Zemepán Nikolaj Palfi", 100, 1950, 40);
+            
+            Player player1 = new Player("Bača Jano", 20, 1500, 100, 5, 80);
+            Player player2 = new Player("Zemepán Nikolaj Palfi", 20, 1600, 100, 5, 80);
 
             while (player2.HP >= 0 && player1.HP >= 0) 
             {
-                Console.WriteLine("Player2 HP: " + player2.HP);
-                player1.DamagePlayer(player2);
-                Console.WriteLine("Player2 HP afret Fight: " + player2.HP);
-                player1.HealPlayer(player1);
-
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Player1 HP after Fight: " + player1.HP);
+                Console.WriteLine("Player1 Hp after heal: " + player1.HP);
+                if (player1.HP <= 20)
+                {
+                    bool isHeald = player1.Heal();
+                    if (isHeald)
+                    {
+                        Console.WriteLine("Kladna postava bola uzdravena");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Kladna postava uz nema manu");
+                    }
+                }
                 Console.WriteLine("Player1 HP: " + player1.HP);
                 player2.DamagePlayer(player1);
-                Console.WriteLine("Player1 HP afret Fight: " + player1.HP);
-                player2.HealPlayer(player2);
+
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Player2 HP: " + player2.HP);
+                player1.DamagePlayer(player2);
+                if (player2.HP <= 20)
+                { 
+                        bool isHeald = player2.Heal();
+                        if (isHeald)
+                        {
+                            Console.WriteLine("Zaporna postava bola uzdravena");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Zaporna postava uz nema manu");
+                        }
+                }
+
+
+                Console.WriteLine("Player2 HP after Fight: " + player2.HP);
+                Console.WriteLine("Player2 Hp after heal: " + player2.HP);
             }
             if (player1.HP >= player2.HP)
             {
+                Console.ForegroundColor = ConsoleColor.Blue;
                 Console.WriteLine("Player1 is winner!");
             }
             else
             {
+                ConsoleColor color = ConsoleColor.Blue;
                 Console.WriteLine("Player2 is winner");
             }
         }
