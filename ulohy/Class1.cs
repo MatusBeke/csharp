47665843b4b9186a5bd7a21fd5811012385fb42a
matusbeke <matus.beke7@gmail.com>
2024-10-22 08:54:11 +0200

    Hotovo :D

---
+++ ulohy/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ulohy
+{
+    public class Rectangle
+    {
+        public Rectangle()
+        {
+
+        }
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public int Width {  get; set; }
+        public int Height { get; set; }
+
+
+        public int Calculate()
+        {
+            int Result = Width * Height;
+           // Console.WriteLine(Result);
+            return Result;
+
+        }
+    }
+
+}
---
+++ ulohy/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ulohy
+{
+    internal class rectangle
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Zadaj rozmery obdlznika: ");
+
+            var width = int.Parse(Console.ReadLine());
+            var height = int.Parse(Console.ReadLine());
+            Rectangle rectangle = new Rectangle(width, height);
+
+            int Result = rectangle.Calculate();
+            Console.WriteLine("Obsah obdlznika je: " + Result);
+
+        }
+    }
+}
